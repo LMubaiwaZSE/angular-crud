ba3f25dbb6ca216a63aaae138bf10e695d4e4d6e
MorabaTech <138768399+LMubaiwaZSE@users.noreply.github.com>
2023-07-18 10:58:32 +0200

    full laravel/ui angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
+import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',component: HomePageComponent,title: 'Home Page'},
+{path:'about-us',component: AboutPageComponent,title: 'About Page'},
+{path:'contact-us',component: ContactPageComponent,title: 'Contact Page'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
+import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
+import { NavBarComponent } from './Pages/Partials/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
